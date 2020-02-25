851da27e0fe3a1d1fc6dc29a2bca04a077b36a39
Gautam Dayal <gautam.dayal@yahoo.com>
2020-02-25 18:21:47 +0530

    added problem

---
+++ dataScienceCourse/week1/problem_gd.py
@@ -0,0 +1,13 @@
+"""
+A 'special number' is one that is:
+    - even
+    - divisible by 3 
+    - greater than 10
+
+In your program, write a program that inputs a number 'n' and then prints out whether
+n is a special number or not. The output format should be:
+    (if it's a special number)
+    Yes, n is a special number
+    (or if it isn't)
+    No, n is not a special number
+"""
